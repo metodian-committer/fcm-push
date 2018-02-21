112c82aa5df4c6a7022602a51bbc8d6b8173e619
Metodian <contact@metodian.com>
2018-02-21 19:10:56 +0100

    JSON-encoding $fields array

--- src/FCMPush.php
+++ src/FCMPush.php
@@ -4,37 +4,36 @@ namespace Sab94\FCMPush;
 
 class FCMPush
 {
-    public static function sendPush($tokens,$message)
+    public static function sendPush($tokens, $message)
     {
         $url = config('push.url');
-        $fields = array(
+        $fields = json_encode([
             'registration_ids' => $tokens,
-            'data' => $message
-        );
-        $headers = array(
+            'data' => ['body' => $message],
+        ]);
+        $headers = [
             'Authorization:key = '.config('push.authorization:key'),
-            'Content-Type: '.config('push.content-type')
-        );
+            'Content-Type: '.config('push.content-type'),
+        ];
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt ($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
         $result = curl_exec($ch);
+
         //Print error if any
         if (curl_errno($ch)) {
             curl_close($ch);
-            return  "Send Push Failed";
 
+            return  'Send Push Failed';
         } else {
             curl_close($ch);
+
             return $result;
         }
-
-
     }
-
 }
